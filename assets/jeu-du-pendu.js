885017632d456c3dee7a24f174219b15cc9fca41
math13001 <mathieu.fremont13@gmail.com>
2023-10-17 16:49:25 +0200

    first commit

--- assets/jeu-du-pendu.js
+++ assets/jeu-du-pendu.js
@@ -1,1 +1,16 @@
 console.log("Script chargé !")
+
+const mesCellules = document.createElement("div")
+      mesCellules.className = "cellules"
+      document.body.appendChild(mesCellules)
+    
+const mesLettres =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+      mesLettres.className = "lettres"
+
+
+
+
+
+
+
+    
